242974cc41b7239803cc942b41773adb5a7430a3
bhavya <bhavyasree.kasa@gmail.com>
2026-04-14 21:26:58 +0530

    clicked on continue

--- AutoQA/src/test/java/tests/RegisterTest.java
+++ AutoQA/src/test/java/tests/RegisterTest.java
@@ -3,6 +3,7 @@ package tests;
 import base.BaseTest;
 import pages.SignupPage;
 import pages.AccountInfoPage;
+import pages.AccountCreatedPage;
 import org.testng.annotations.Test;
 import utils.TestData;
 
@@ -21,8 +22,11 @@ public class RegisterTest extends BaseTest {
         signup.clickSignup();
 
         AccountInfoPage account = new AccountInfoPage(driver);
-
         account.fillAccountDetails();
         account.clickCreateAccount();
+
+        // 🔥 NEW STEP
+        AccountCreatedPage createdPage = new AccountCreatedPage(driver);
+        createdPage.clickContinue();
     }
 }
